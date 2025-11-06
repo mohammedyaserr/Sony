a87f9bac08be0dab63ed820a1fa78418e86f6a65
mohammedyaserr <yasermohammed687@gmail.com>
2025-11-06 17:09:54 +0530

    footernote

---
+++ Frontend/src/Components/Home-page/Home-footnotes.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Homefootnotes = () => {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default Homefootnotes
--- Frontend/src/Components/Home-page/Home-recently-viewed.jsx
+++ Frontend/src/Components/Home-page/Home-recently-viewed.jsx
@@ -2,19 +2,27 @@ import React from 'react'
 
 import './Home-recently-viewed.css'
 
+import recentlyviewed from '../../assets/Primary_image_black.webp'
+
 const Homerecentlyviewed = () => {
-  return (
-    <>
-      <div className="home-recently-viewed">
-        <div className="home-recently-viewed-inner">
-            <h2>RECENTLY VIEWED PRODUCTS</h2>
-            <div className="home-recently-viewed-container">
-                
+    return (
+        <>
+            <div className="home-recently-viewed">
+                <div className="home-recently-viewed-inner">
+                    <h2>RECENTLY VIEWED PRODUCTS</h2>
+                    <div className="home-recently-viewed-container">
+                        <div className="home-recently-viewed-item">
+                            <div className="home-recently-viewed-item-img">
+                                <img src={recentlyviewed} alt="" />
+                            </div>
+                            <p>WF-1000XM5 Wireless Noise Cancelling Headphones</p>
+                            <h2>WF-1000XM5</h2>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
-      </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default Homerecentlyviewed
