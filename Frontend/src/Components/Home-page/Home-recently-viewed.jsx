f89d20211475c16fc3ed1df65cca15369b085e16
mohammedyaserr <yasermohammed687@gmail.com>
2025-11-06 15:00:26 +0530

    home promotion and offers ui update

--- Frontend/src/Components/Home-page/Home-recently-viewed.jsx
+++ Frontend/src/Components/Home-page/Home-recently-viewed.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 
+import './Home-recently-viewed.css'
+
 const Homerecentlyviewed = () => {
   return (
     <>
       <div className="home-recently-viewed">
         <div className="home-recently-viewed-inner">
-            
+            <h2>RECENTLY VIEWED PRODUCTS</h2>
+            <div className="home-recently-viewed-container">
+                
+            </div>
         </div>
       </div>
     </>
--- Frontend/src/Components/Pages/Home-page.jsx
+++ Frontend/src/Components/Pages/Home-page.jsx
@@ -4,6 +4,7 @@ import HomeBanner from '../Home-page/Home-Banner'
 import HomeNotices from '../Home-page/Home-Notices'
 import Homelatest from '../Home-page/Home-Latest'
 import Homepromotionsandoffers from '../Home-page/Home-promotions-and-offers'
+import Homerecentlyviewed from '../Home-page/Home-recently-viewed'
 
 const Homepage = () => {
   return (
@@ -13,6 +14,7 @@ const Homepage = () => {
       <HomeNotices/>
       <Homelatest/>
       <Homepromotionsandoffers/>
+      <Homerecentlyviewed/>
     </>
   )
 }
