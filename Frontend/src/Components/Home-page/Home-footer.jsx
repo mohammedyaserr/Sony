f1324f8e7b66b37f1e4588ff0783aeb76fd55b9f
mohammedyaserr <yasermohammed687@gmail.com>
2025-11-10 18:18:24 +0530

    footer ui update

--- Frontend/src/Components/Home-page/Home-footer.jsx
+++ Frontend/src/Components/Home-page/Home-footer.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 import './Home-footer.css'
 
+import earth from '../../assets/globe-earth.png'
+
 const Homefooter = () => {
     return (
         <>
@@ -16,11 +18,45 @@ const Homefooter = () => {
                             </div>
 
                             <div className="home-footer-container-inner1-search">
-                                {/* <img src="" alt="" /> */}
-                                <input type="text" />
+                                <img src={earth} alt="" />
+                                <input type="text" placeholder='Location'/>
                                 <button>Search</button>
                             </div>
                             <p className='home-footer-loc-para'>See Our Locations</p>
+                        </div>
+
+
+                    {/* ------- sec- section ------- */}
+
+                    <div className="home-footer-container-inner1-first">
+
+                            <div className="home-footer-container-inner1-para">
+                                <p>Support</p>
+                            </div>
+                            <p className='home-footer-loc-para'>Online Help</p>
+                            <p className='home-footer-loc-para'>Track Repair Status</p>
+                            <p className='home-footer-loc-para'>Locate Service Centers</p>
+                        </div>
+
+                        {/* ------- third- section ------- */}
+
+                    <div className="home-footer-container-inner1-first">
+
+                            <div className="home-footer-container-inner1-para">
+                                <p>In the news</p>
+                            </div>
+                            <p className='home-footer-loc-para'>Press center</p>
+                        </div>
+
+                        {/* ------- fourth- section ------- */}
+
+                    <div className="home-footer-container-inner1-first">
+
+                            <div className="home-footer-container-inner1-para">
+                                <p>Contact Us</p>
+                            </div>
+                            <p className='home-footer-loc-para'>Newsletter Subscriptions</p>
+                            <p className='home-footer-loc-para'>Contact Us</p>
                         </div>
                     </div>
 
