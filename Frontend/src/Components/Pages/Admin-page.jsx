22750679c30777b71328cc4b3d9e39036805e7d8
mohammedyaserr <yasermohammed687@gmail.com>
2025-11-16 18:29:06 +0530

    admin dashboard

--- Frontend/src/Components/Pages/Admin-page.jsx
+++ Frontend/src/Components/Pages/Admin-page.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Adminnav from '../Admin-page/Admin-nav'
+import MiniDrawer from '../Admin-page/Admin-nav'
+import ResponsiveDrawer from '../Admin-page/Admin-nav'
 
 const Adminpage = () => {
   return (
     <>
-      <Adminnav/>
+      {/* <Adminnav/> */}
+
+      <ResponsiveDrawer/>
     </>
   )
 }
