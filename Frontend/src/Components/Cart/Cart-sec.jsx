64a803c0316f7fb83543294a27493ec427fd17cd
mohammedyaserr <yasermohammed687@gmail.com>
2025-12-16 00:52:09 +0530

    feat: enhance cart section with product details and quantity controls

--- Frontend/src/Components/Cart/Cart-sec.jsx
+++ Frontend/src/Components/Cart/Cart-sec.jsx
@@ -2,7 +2,9 @@ import React from "react";
 
 import './cart-sec.css';
 
-import cartitem from  '../../assets/Promotion_tile_4span_D-12.webp'
+import cartitem from '../../assets/Promotion_tile_4span_D-12.webp'
+import minusicon from '../../assets/minus-sign.png'
+import plusicon from '../../assets/plus.png'
 
 const CartSec = () => {
     return (
@@ -17,7 +19,21 @@ const CartSec = () => {
                         </div>
 
                         <div className="cart-prdtdetails-container">
-                            
+                            <div className="cart-prdts">
+                                
+                                <div className="cart-prtd-txt">
+                                <h1>Product Name</h1>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta incidunt nulla, aut necessitatibus in minus eius vitae dolor sed numquam fuga eligendi. Perferendis facere perspiciatis aliquam impedit molestias saepe ut.</p>
+
+                                </div>
+
+                                <div className="prtd-qnty">
+                                    <img src={minusicon} alt="" />
+                                    <p>2</p>
+                                    <img src={plusicon} alt="" />
+                                </div>
+                                <h2>12,999 rs</h2>
+                            </div>
                         </div>
                     </div>
 
